68eec18711eef84aa0bb3b3e0c729cb9ee219f7e
Marie <Marie@Marie-HP>
2015-10-05 23:07:44 +0200

    Merge origin/master
    Conflicts:
    	src/Wybe/FrontOfficeBundle/Resources/views/Default/signup-details-form.html.twig

--- src/Wybe/FrontOfficeBundle/Controller/DefaultController.php
+++ src/Wybe/FrontOfficeBundle/Controller/DefaultController.php
@@ -27,8 +27,44 @@ class DefaultController extends Controller
         return $this->render('WybeFrontOfficeBundle:Default:index.html.twig', array('name' => $name));
     }
     
+    public function getParametersAction()
+    {
+        return $this->render('WybeFrontOfficeBundle:Default:parameters.html.twig');
+    }
+        
     public function getEventAction($eventNameSlug, Request $oRequest)
     {
         return $this->render('WybeFrontOfficeBundle:Default:event.html.twig', array('name' => $eventNameSlug));
     }
+    
+    public function getCreateEventAction()
+    {
+        return $this->render('WybeFrontOfficeBundle:Default:create-event.html.twig');
+    }
+    
+    public function getLookForEventAction()
+    {
+        return $this->render('WybeFrontOfficeBundle:Default:look-for-event.html.twig');
+    }
+    
+    public function getGroupAction($groupNameSlug, Request $oRequest)
+    {
+        return $this->render('WybeFrontOfficeBundle:Default:group.html.twig', array('name' => $groupNameSlug));
+    }
+    
+    public function getCreateGroupAction()
+    {
+        return $this->render('WybeFrontOfficeBundle:Default:create-group.html.twig');
+    }
+    
+    public function getLookForGroupAction()
+    {
+        return $this->render('WybeFrontOfficeBundle:Default:look-for-group.html.twig');
+    }
+    
+    public function getNotifAction($notifNameSlug, Request $oRequest)
+    {
+        return $this->render('WybeFrontOfficeBundle:Default:notif.html.twig', array('name' => $notifNameSlug));
+    }   
 }
+
